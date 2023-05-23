1faa70a875a4a4dcc3f342a9128de2503a881cbb
Jorge Vilca <jlvilcav@gmail.com>
2023-05-22 21:27:11 -0500

    se creo la estructura para el modulo de Producto

---
+++ app/Models/Producto/CategoriaProducto.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Producto;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoriaProducto extends Model
+{
+    use HasFactory;
+}
